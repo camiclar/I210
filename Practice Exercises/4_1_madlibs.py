76482e3dece36b5779be39670668c6bfa6c5ba42
camiclar <camiclar@iu.edu>
2023-08-28 14:28:31 -0400

    complete exercise 4.2

--- Practice Exercises/4_1_madlibs.py
+++ Practice Exercises/4_1_madlibs.py
@@ -28,8 +28,8 @@ family_member = input("Enter a family member, e.g. cousin: ")
 # 1A - PROCESS / OUTPUT using .format()
 # each keyword matches to a placeholder in the madlib story
 # print out the result to read your completed madlib
-print(madlib.format(job_title = job_title, noun = noun, volume_level = volume_level, face_part = face_part, adjective1 = adjective1, place = place, drink = drink, adjective2 = adjective2, animal = animal, adjective3 = adjective3, family_member = family_member))
 
+print(madlib.format(job_title = job_title, noun = noun, volume_level = volume_level, face_part = face_part, adjective1 = adjective1, place = place, drink = drink, adjective2 = adjective2, animal = animal, adjective3 = adjective3, family_member = family_member))
 
 # 1B - WARNING: If you want to use f strings, you need to move the SETUP
 # template to be UNDER the inputs. Then add the f to the template.
--- Practice Exercises/4_2_state_facts.py
+++ Practice Exercises/4_2_state_facts.py
@@ -8,18 +8,23 @@ IN_state = {"name": "Indiana",
             "flower": "peony"}
 # 1 - create a dictionary for another state
 # 2 - add facts about its name, state motto, bird and flower
-
+KY_state = {"name": "Kentucky",
+            "motto": "United we stand, divided we fall",
+            "bird": "cardinal",
+            "flower": "goldenrod"}
 
 # INPUT
 # 3 - tell the user what states you will be comparing
 # 4 - ask the user for a state fact (assume it is motto, bird or flower)
-
+print(f"Comparing state facts for {IN_state['name']} and {KY_state['name']}:")
+fact = input("Which state fact would you like to compare (motto, bird or flower)?:\n")
 
 # PROCESSING / OUTPUT
 # 5 - tell the user what the motto, bird or flower is for each state
 #   - (this is easier with f strings)
-
+print(f"The {IN_state['name']} state {fact} is {IN_state[fact]}")
+print(f"The {KY_state['name']} state {fact} is {KY_state[fact]}")
 
 # 6 - then compare the facts for both states
 # 7 - answer the question, are they the same for both states?
-
+print(f"Is the {fact} the same for both? {IN_state[fact] == KY_state[fact]}")
