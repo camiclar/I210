f42a56845a3c1c356bc6e0ebf26481c8718bcb73
camiclar <camiclar@iu.edu>
2023-09-27 14:24:42 -0400

    class 9/27

--- Practice Exercises/11_3_counting_coins_functions.py
+++ Practice Exercises/11_3_counting_coins_functions.py
@@ -6,56 +6,59 @@ import random
 # and returns a list of coins (listed by name)
 # e.g. ['dime', 'quarter', ...]
 
+def create_cup(num_coins):
+    coin_types = ["penny", "nickel", "dime", "quarter"]
+    change = []
+
+    for i in range(num_coins):
+        change.append(random.choice(coin_types))
+
+    return change
+
 # (2) write a function "count_coins" 
 # that takes in a list of coins,
 # counts the coins and returns a dictionary
 # results stored as {type: amount}, e.g. {'dime': 16, ...}
+
+def count_coins(coins):
+    counted_coins = {}
+
+    for coin in coins:
+        if coin not in counted_coins:
+            counted_coins[coin] = 1
+        else:
+            counted_coins[coin] += 1
+
+    return counted_coins
 
 # (3) write a function "print_change" 
 # that takes in a dictionary of coins,
 # and prints the summary about them.
 # There is no need for a return statement!
 
+def print_change(coins):
+    quarters = coins["quarter"] * 0.25
+    dimes = coins["dime"] * 0.10
+    nickels = coins["nickel"] * 0.05
+    pennies = coins["penny"] * 0.01
+
+    total = quarters + dimes + nickels + pennies
+
+    print('Quarters: ${:.2f}'.format(quarters))
+    print('Dimes: ${:.2f}'.format(dimes))
+    print('Nickels: ${:.2f}'.format(nickels))
+    print('Pennies: ${:.2f}'.format(pennies))
+
+    print('\nTotal change in cup: ${:.2f}'.format(total))
+    
 
 # SET-UP
-types_of_coins = ["penny", "nickel", "dime", "quarter"]
-
 # main
 
-# CREATE A CUP FULL OF CHANGE
-change = []
-for i in range(50):
-    change.append(random.choice(types_of_coins))
+if __name__ == "__main__":
+    num_coins = int(input("Input how many coins are in the cup: "))
+    print()
 
-# print(change) 
-
-# COUNT HOW MANY COINS OF EACH TYPE
-counted_coins = {}
-for coin in change:
-    # if that type of coin isn't in the dictionary, add it
-    if coin not in counted_coins:
-        counted_coins[coin] = 1
-    # if that type of coin is in the dictionary, add to the value
-    else:
-        counted_coins[coin] += 1
-
-# print(counted_coins)
-
-# CALCULATE THE MONETARY VALUE
-# Question: how many more of these kinds of calculations
-# and printing statements would we need before it makes
-# sense to also make functions to handle these tasks?
-
-quarters = counted_coins["quarter"] * 0.25
-dimes = counted_coins["dime"] * 0.10
-nickels = counted_coins["nickel"] * 0.05
-pennies = counted_coins["penny"] * 0.01
-
-total = quarters + dimes + nickels + pennies
-
-print('Quarters: ${:.2f}'.format(quarters))
-print('Dimes: ${:.2f}'.format(dimes))
-print('Nickels: ${:.2f}'.format(nickels))
-print('Pennies: ${:.2f}'.format(pennies))
-
-print('\nTotal change in cup: ${:.2f}'.format(total))
+    cup = create_cup(num_coins)
+    counted_coins = count_coins(cup)
+    print_change(counted_coins)
