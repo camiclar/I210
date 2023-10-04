abb8fd746c0a5f19776509be81dbe721159efadd
camiclar <camiclar@iu.edu>
2023-10-04 14:29:45 -0400

    class 10/4

--- Practice Exercises/13_2_tiger_speak.py
+++ Practice Exercises/13_2_tiger_speak.py
@@ -1,5 +1,5 @@
 # 13.2 Tiger speak
-
+import random
 
 # Write a function "tiger_speak":
 # (takes a string, returns a string)
@@ -7,11 +7,36 @@
 # Each character is **randomly** upper or lower case
 # Every space is now a dash - tigers have stripes!
 
+def tiger_speak(sentence):
+    output_sentence = ""
+
+    for i in range(len(sentence)):
+        is_upper = random.randint(0, 1)
+        if is_upper:
+            output_sentence += sentence[i].upper()
+        else:
+            output_sentence += sentence[i].lower()
+
+    output_sentence = output_sentence.replace(" ", "-")
+
+    return output_sentence
 
 
 
 
 # main
+if __name__ == "__main__":
+    sentence1 = "Tyger Tyger, burning bright,"
+    sentence2 = "In the forests of the night,"
+    sentence3 = "What immortal hand or eye,"
+    sentence4 = "Could frame thy fearful symmetry?"
+
+    print(tiger_speak(sentence1))
+    print(tiger_speak(sentence2))
+    print(tiger_speak(sentence3))
+    print(tiger_speak(sentence4))
+
+
 
 # Make your sentences wild like a tiger:
 # Get four sentences from the user and put them in a list
--- Practice Exercises/13_3_valid_zip_update.py
+++ Practice Exercises/13_3_valid_zip_update.py
@@ -5,15 +5,23 @@ def valid_zip(is_zip):
     #   if zip code is 5 characters long OR 10 characters long
     #   if zip is 5 -- make sure it's all digits
     #   if zip is 10 -- make sure it's 5 digits, one + sign, and 4 digits
-    return len(is_zip) == 5
+    
+    is_valid = False
+    if len(is_zip) == 5:
+        is_valid = is_zip.isdigit()
+    elif len(is_zip) == 10:
+        is_valid = is_zip[0:4].isdigit() and is_zip[5] == "+" and is_zip[6:].isdigit()
+    
+    return is_valid
+    
 
 # customized location info for each valid zip code
 def find_zip(is_zip):
     if not valid_zip(is_zip):
-        return ""
+        return "That's not a valid zip code."
 
     # update: checks only the base zipcode a.k.a. the first five numbers
-    if 47401 <= int(is_zip) <= 47408:
+    if 47401 <= int(is_zip[0:5]) <= 47408:
         zone = "Bloomington"
     else:
         zone = "your hometown"
