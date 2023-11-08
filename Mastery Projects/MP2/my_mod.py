004b0b4e6329c87d8827e1c6e840f34bb594a86f
camiclar <camiclar@iu.edu>
2023-11-07 22:20:35 -0500

    finish part 2

--- Mastery Projects/MP2/my_mod.py
+++ Mastery Projects/MP2/my_mod.py
@@ -13,15 +13,28 @@ def load_data(file_name):
             for row in spotify_reader:
                 empty_values = [value for value in row.values() if not value] # Checks all values in row; if a value is empty, add to list
                 if not empty_values: # If there are no empty values
-                    songs.append(row["track_name"])
+                    songs.append(row)
 
         # Remove duplicate entries
         unique_songs = []
+        track_names = []
         
         for song in songs:
-            if song not in unique_songs:
+            if song["track_name"] not in track_names:
+                track_names.append(song["track_name"])
                 unique_songs.append(song)
 
+        # Convert numerical values to correct data types
+        for song in unique_songs:
+            for feature, value in song.items():
+                try:
+                    song[feature] = int(value)
+                except ValueError:
+                    try:
+                        song[feature] = float(value)
+                    except ValueError:
+                        pass
+        
         # Return it!
         return unique_songs
 
@@ -32,16 +45,27 @@ def load_data(file_name):
 # PART 2: Audio analysis function, sort function, table_print function
 # By default, print the 'top 20'
 def track_analysis(track_list, feature, cutoff = 20):
-    # Use track_list to compile the data you need
+    try:
+        track_data = []
 
-    # Sort it
+        # Use track_list to compile the data you need
+        for track in track_list:
+            
+            track_data.append((track["track_name"], track["artist_name"], track[feature]))
+        
+        # Sort it
+        insertion_sort(track_data, 2)
+        track_data.reverse()
 
-    # 'Chop off' extra data
+        # 'Chop off' extra data
+        table_data = [track_data[i] for i in range(0, cutoff)]
 
-    # Print it as a table using table_print()
+        # Print it as a table using table_print()
+        table_print(["track_name", "artist_name", feature], table_data)
 
-    # You can remove this when you start working on this function
-    pass
+    except KeyError:
+        print(f"No feature called {feature}")
+
 
 # PART 4: BONUS - Artist info function
 def artist_info(track_list, artist_name):
@@ -51,15 +75,19 @@ def artist_info(track_list, artist_name):
     pass
 
 # Implementation of Selection Sort or Insertion Sort for Part 2 and Part 3
-def your_sort(nested_list, pos):
-    # Modify the one from class to work on column pos (position)
-
+def insertion_sort(nested_list, pos):
     # Sort the list
+    for i in range(1, len(nested_list)):
+        j = i
+
+        while j > 0 and nested_list[j][pos] < nested_list[j - 1][pos]:
+            temp = nested_list[j]
+            nested_list[j] = nested_list[j - 1]
+            nested_list[j - 1] = temp
+            j = j - 1
 
     # Return it
-    
-    # You can remove this when you start working on this function
-    pass
+    return nested_list
 
 # Implementation of Table Print for multi-column tables.
 # You can have as many header columns as needed.
@@ -68,11 +96,12 @@ def table_print(headers, data, width = 30):
     # Modify the one from class to work on 3 columns instead of just 2
 
     # Print the Headers
+    print(f"{headers[0]:<{width}}{headers[1]:<{width}}{headers[2]:<{width}}")
+    print("-" * (width * 3))
 
     # Print the Data
-
-    # You can remove this when you start working on this function
-    pass
+    for item in data:
+        print(f"{item[0]:<{width}}{item[1]:<{width}}{item[2]:<{width}}")
 
 
 # Test code for the Module
@@ -86,7 +115,7 @@ if __name__ == "__main__":
     # TEST PART 2A
     instruments = [("Trumpet", "Brass", 35), ("Saxophone", "Woodwind", 12),
                    ("Snare Drum", "Percussion", 22), ("Cymbal", "Percussion", 40)]
-    sorted_instruments = your_sort(instruments, 2)
+    sorted_instruments = insertion_sort(instruments, 2)
     print(sorted_instruments)
     print()
 
