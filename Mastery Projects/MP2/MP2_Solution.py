2fb03a7c4a51ad3d6453c27b46c919fab36a7036
camiclar <camiclar@iu.edu>
2023-11-08 20:50:05 -0500

    finish part 3 (almost)

--- Mastery Projects/MP2/MP2_Solution.py
+++ Mastery Projects/MP2/MP2_Solution.py
@@ -3,41 +3,56 @@ import my_mod
     
 # PART 4: Main
 
-# Use your module to load the data from spotify_data_2022.csv
-# HERE
 print("Welcome to Spotify Data Analysis!\n")
-#file_path = input("Enter the file path for your data: ") #TODO: Uncomment when done testing
-file_path = "spotify_data_2022.csv"
 
+file_path = input("Enter the file path for your data: ") #TODO: Uncomment when done testing
+#file_path = "spotify_data_2022.csv" # TODO: Delete when done testing
+
+# Issues:
+# : or ;
+# Empty string
+
+# Error handling for file path
 while not my_mod.load_data(file_path):
     file_path = input("\nPlease try again: ")
 
 data = my_mod.load_data(file_path)
 
+# Menu
 while True:
-    # Print our Menu
+    # Print out Menu
     print("\n\tType '1' for audio analysis\n")
     print("\tType '2' for artist info\n")
     print("\tType 'quit' to exit the program\n")
 
     # Get user input
-    answer = input("What would you like to do? ")
+    answer = input("What would you like to do? ") # TODO: Error handling
 
     # Allow the user to quit
     if answer.lower() == "quit":
         break
 
-    # Allow the user to perform a track feature analysis
+    # Track feature analysis
     elif answer == "1":
-        print("\nTrack features include things like track_danceability, track_loudness, etc.")
-        #user_feature = input("What track feature would you like to sort by? ")
 
-        num_tracks = 15 # TODO: Set to 0 when done testing
+        # Track feature
+        print("\nTrack features include things like track_danceability, track_loudness, etc.")
+
+        feature = input("What track feature would you like to sort by? ") #TODO: Uncomment when done testing
+        #feature = "track_popularity" # TODO: Remove when done testing
+        
+        # Error handling for track feature
+        while feature not in data[0].keys():
+            feature = input(f"No feature called {feature}. Please try again: ")
+
+        # Set number of tracks to display
+        num_tracks = 0 # TODO: Set to 0 when done testing
         try:
-            #num_tracks = int(input("How many top tracks would you like to display? ")) #TODO: Uncomment when done testing
+            num_tracks = int(input("How many top tracks would you like to display? ")) #TODO: Uncomment when done testing
             if num_tracks < 1 or num_tracks > len(data): # Makes sure number is in range
                 raise ValueError()
-            
+
+        # Exception Handling for Number of Tracks
         except ValueError:
             while True: # Repeats until valid number is entered
                 print(f"\nNumber of top tracks must be a digit between 1-{len(data)}.")
@@ -50,6 +65,9 @@ while True:
                 
                 except ValueError:
                     pass
+
+        # Print data
+        my_mod.track_analysis(data, feature, num_tracks)
 
 
     # Allow the user to perform an artist info analysis (BONUS)
